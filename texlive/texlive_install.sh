36d4a4908245810663667398aea28d382b800902
Thomas <hollandpirates@gmail.com>
2018-11-03 20:07:53 +0100

    Try cd from yml

--- texlive/texlive_install.sh
+++ texlive/texlive_install.sh
@@ -16,10 +16,10 @@ if ! command -v texlua > /dev/null; then
   cd install-tl-20*
 
   # Find directory this file is in, to find the texlive.profile file.
-  BASEDIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+#  BASEDIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
   # Install a minimal system
-  ./install-tl --profile="$BASEDIR"/texlive.profile
+  ./install-tl --profile=./texlive.profile
 
   cd ..
 fi
